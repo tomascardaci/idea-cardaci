ce34dd86e26c71d01416b77a83d2bca58010f3ec
tomascardaci <tomascardaci99@gmail.com>
2022-06-25 18:34:25 -0300

    desafio_clase_2

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <div className='NavBar'>
+            <ul>
+                <li>Inicio</li>
+                <li>Catálogo</li>
+                <li>Contacto</li>
+                <li>Perfil</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
